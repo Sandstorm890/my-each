d1a37042c7cd8e0d70fb4fe1bf60596c61d0b418
zaynegoering <zaynegoering@gmail.com>
2020-10-22 10:26:25 -0700

    commit tree 881802e5defc9952032fc6ce9d6fccd209166da8

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
-  # code here
+x = [1, 2, 3, 4]
+
+def my_each(array)
+  counter = 0
+  new_array = []
+  while counter < array.length
+    yield array[counter]
+    counter += 1
+  end
+  array
 end
+
+my_each(x) do |num|
+  num
+end
+
